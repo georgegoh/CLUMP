f72d1c841a59c1ded24749ac9189123d5c872704
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-04-20 07:13:12 +0000

    Removed

--- src/modules/installer/src/__init__.py
+++
@@ -1,8 +0,0 @@
-# $Id: __init__.py 237 2007-04-05 08:57:10Z ggoh $
-#
-# Kusu Text Installer Screen Package.
-#
-# Copyright 2007 Platform Computing Corporation.
-#
-# Licensed under GPL version 2; See LICENSE file for details.
-#
