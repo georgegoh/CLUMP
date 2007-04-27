198184a19d1335b173d8500d195257c620518e6e
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-04-27 05:44:43 +0000

    Left out the distro dir

--- src/modules/util/setup.py
+++ src/modules/util/setup.py
@@ -19,7 +19,7 @@ setup(name="kusu-util",
     platforms=["any"],
     packages=['util', 'util.distro'],
     package_dir={'util':'src/lib',
-                 'util.distro': 'src/lib/util'},
+                 'util.distro': 'src/lib/distro'},
     data_files=[('etc', ['src/etc/distro.conf']),
                 ('share/doc/samples', ['src/doc/samples/verify-fc6-repo'])] 
      )
