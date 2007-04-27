3d3f204ea3210042e702ecf09fbce6872c8f0120
mike <mike@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-04-27 04:00:32 +0000

    Edit log module entries

--- src/modules/util/setup.py
+++ src/modules/util/setup.py
@@ -17,10 +17,9 @@ setup(name="kusu-util",
     author_email="najib@osgdc.org",
     url="http://www.osgdc.org/project/kusu",
     platforms=["any"],
-    packages=['util', 'util.distro', 'util.log'],
+    packages=['util', 'util.distro'],
     package_dir={'util':'src/lib',
-                 'util.distro': 'src/lib/distro',
-                 'util.log': 'src/lib'},
+                 'util.distrp': 'src/lib/util'},
     data_files=[('etc', ['src/etc/distro.conf']),
                 ('share/doc/samples', ['src/doc/samples/verify-fc6-repo'])] 
      )
