d90ca7bd89630af55907363ec60d29df64fb7242
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-05-01 13:19:05 +0000

    initial checkin of network screen

--- src/modules/installer/src/lib/kusufactory.py
+++ src/modules/installer/src/lib/kusufactory.py
@@ -23,6 +23,7 @@ from gatewaydns import GatewayDNSSetupScreen
 from kits import KitsScreen
 from tzselect import TZSelectionScreen
 from confirm import ConfirmScreen
+from network import NetworkScreen
 
 class ScreenImpl(BaseScreen):
     """This class is a template for other Screen classes."""
@@ -56,6 +57,7 @@ class ScreenFactoryImpl(ScreenFactory):
                              RootPasswordScreen(collection, kusuApp=runtimeDict),
                              PartitionScreen(collection, kusuApp=runtimeDict),
                              KitsScreen(collection, kusuApp=runtimeDict),
+                             NetworkScreen(collection, kusuApp=runtimeDict),
 #                             GatewayDNSSetupScreen(collection, kusuApp=runtimeDict),
                              TZSelectionScreen(collection, kusuApp=runtimeDict),
                              ConfirmScreen(collection, kusuApp=runtimeDict)
---
+++ src/modules/installer/src/lib/network.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# $Id$
+#
+# Kusu Text Installer Network Screen.
+#
+# Copyright 2007 Platform Computing Corporation.
+#
+# Licensed under GPL version 2; See LICENSE file for details.
+#
+__version__ = "$Revision: 237 $"
+
+import socket
+import logging
+import snack
+from gettext import gettext as _
+from kusu.ui.text import screenfactory, kusuwidgets
+from kusu.ui.text.kusuwidgets import LEFT,CENTER,RIGHT
+from kusu.hardware import net
+
+class NetworkScreen(screenfactory.BaseScreen):
+    """This is the network screen."""
+    name = _('Network')
+    context = 'Network'
+    msg = _('Please configure your network')
+    buttons = []
+
+    def drawImpl(self):
+        self.screenGrid = snack.Grid(1, 2)
+        
+        instruction = snack.Label(self.msg)
+        self.screenGrid.setField(instruction, col=0, row=0)
+
+        self.listbox = snack.Listbox(8, scroll=1, returnExit=1)
+        for intf in net.getInterfaces():
+            intf = net.Net(intf)
+            if intf.isPhysical():
+                self.listbox.append('%s - %s %s' % (intf.interface, intf.vendor, intf.device), intf)
+
+        self.screenGrid.setField(self.listbox, col=0, row=1,
+                                 padding=(0,1,0,-1))
+
+
+
