d1b7670d495d6ecbd07f718072da1b59f2c653d0
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-05-02 14:49:28 +0000

    initial checkin to detect dmraid volumes

---
+++ src/modules/hardware/src/lib/dm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# $Id$
+#
+# Copyright 2007 Platform Computing Corporation.
+#
+# Licensed under GPL version 2; See LICENSE file for details.
+#
+
+from path import path
+import subprocess
+
+def getRAID():
+    # list of devices under raid
+    #dmraid -b -c 
+
+
+    dmraidP = subprocess.Popen('dmraid -s -c', 
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = dmraidP.communicate()
+
+    returncode = dmraidP.returncode
+
+    if not returncode:
+        return [ str(path('/dev/mapper') / d)for d in out.split() ]
+    
+    
