af85b64f7d5e1b716f6618e5edb7f528a2c66897
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-05-08 08:22:52 +0000

    Sync up with lvm

--- src/modules/autoinstall/src/lib/scriptfactory.py
+++ src/modules/autoinstall/src/lib/scriptfactory.py
@@ -29,14 +29,6 @@ class BaseFactory:
 class KickstartFactory(BaseFactory):
     
     def getNameSpace(self):
-        #self.namespace['url'] = self.profile.getInstallSRC()
-        #self.namespace['rootpw'] = self.profile.getRootPw()
-        #self.namespace['tz'] = self.profile.getTZ()
-        #self.namespace['lang'] = self.profile.getAttr('lang')
-        #self.namespace['keybd'] = self.profile.getAttr('keybd')
-        #self.namespace['packages'] = self.profile.getPackageProfile()
-        #self.namespace['partitions'] = self._getPartitions()
-
         self.namespace['url'] = self.profile.installsrc
         self.namespace['rootpw'] = self.profile.rootpw
         self.namespace['tz'] = self.profile.tz
@@ -63,15 +55,18 @@ class KickstartFactory(BaseFactory):
                         'linux-swap': 'swap',
                         None: None}
 
-        #disk_profile = self.profile.getDiskProfile()
+        # Handles normal partitions without, pv, vg, lv 
         disk_profile = self.profile.diskprofile
         for disk in disk_profile.disk_dict.values():
             for id, p in disk.partition_dict.items():
                 fs_type = fstype_dict[p.fs_type]
-                #print id, p.path, p.mountpoint, fs_type, p.type
 
                 # Ignore other type of partitions             
                 if p.type.lower() in ['extended']: 
+                    continue
+                
+                # Ignore physical volume
+                if p.lvm_flag:
                     continue
 
                 # Ignore empty partitions
@@ -96,46 +91,32 @@ class KickstartFactory(BaseFactory):
 
                 part_lines.append(str) 
 
-        return part_lines
 
+        # Handles LVM
+
+        # Dictionary for kickstart pv.<id> for physical volumes
+        # and vol ggroup association
         pv_id = {}  
-        p = []
-            
+     
         cnt = 1
-        disk_profile = self.profile.getDiskProfile()
-        for disk in disk_profile.getDisk():
-            for part in disk.getPart():
-                if part.part_id == '8e':
-                    str = 'part pv.%d --onpart=%s'  % (cnt,part.dev)
-                    pv_id[part.dev] = cnt                  
-                    p.append(str)
+        #PV
+        for pv in disk_profile.pv_dict.values(): 
+            str = 'part pv.%d --onpart=%s'  % (cnt,
+                   os.path.sep.join((path(pv.partition.path).splitall()[2:])))
+            pv_id[pv.partition.path] = cnt                  
+            part_lines.append(str)
 
-        for vg in disk_profile.getVolGrp():
-            str = 'volgroup %s %s' % (vg.name, ' '.join(['pv.%s' % pv_id[part.dev] for part in vg.getPart()]))
-            p.append(str)
-        
-        for vol in disk_profile.getVol():
-            if isinstance(vol.obj, Partition):
-                str = 'part %s' % vol.mntpoint
-                if vol.fstype is not None:
-                    str = str + ' --fstype=%s' % vol.fstype
-
-                if vol.obj.dev is not None:
-                    str = str + ' --onpart=%s' % vol.obj.dev 
-            
-                #if vol.obj.size is not None:
-                #   str = str + ' --size=%s' % vol.obj.size
-
-                str = str + ' --noformat'
-
-            elif isinstance(vol.obj, LogVol):
-                str = 'logvol %s --vgname=%s --name=%s --fstype=%s --size=%s' % \
-                      (vol.mntpoint, vol.obj.volgrp_name, vol.obj.name, vol.fstype, vol.obj.size)
-                
-            p.append(str)
-
-        
-        return p
+        #VG
+        for vg in disk_profile.lvg_dict.values(): 
+            str = 'volgroup %s %s' % (vg.name, ' '.join(['pv.%s' % \
+                  pv_id[pv.partition.path] for pv in vg.pv_dict.values()]))
+            part_lines.append(str)
+       
+        #LV
+        for lv in disk_profile.lv_dict.values(): 
+            str = 'logvol $s --vgname=%s --name=%s --noformat' % 
+                  (lv.mountpoint, lv.group.name, lv.name)
+            part_lines.append(str)
 
 
-
+        return part_lines
