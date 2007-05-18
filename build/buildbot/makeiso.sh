09e4b3a01f2c9dc1f13841318de0d56e3a4dc2e0
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-05-18 08:49:34 +0000

    change rsync destination

--- build/buildbot/makeiso.sh
+++ build/buildbot/makeiso.sh
@@ -8,5 +8,5 @@ boot-media-tool make-patch kususrc=${CMAKE_CURRENT_SOURCE_DIR} os=${KUSU_BUILD_D
 boot-media-tool make-iso kususrc=${CMAKE_CURRENT_SOURCE_DIR} source=${KUSU_DISTRO_SRC} arch=${KUSU_BUILD_ARCH} iso=${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.iso  patch=updates.img
 rm -f updates.img
 tar -czf ${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.src.tgz ${CMAKE_CURRENT_SOURCE_DIR}
-rsync -av --rsh=ssh --delete ${KUSU_BUILD_DIST} build@ronin:/home/osgdc/www/pub/build/.
+rsync -av --rsh=ssh --delete ${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH} build@ronin:/home/osgdc/www/pub/build/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/.
 
