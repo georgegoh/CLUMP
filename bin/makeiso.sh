05a4235ec4091c1f73bb9c913f8b42b692da0c01
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-05-18 02:05:22 +0000

    archive the svn src tree after making iso

--- bin/makeiso.sh
+++ bin/makeiso.sh
@@ -2,7 +2,9 @@
 
 source ${KUSU_ROOT}/bin/kusudevenv.sh
 BUILDDATE=`date +"%Y%m%d%H%m%S"`
-KUSUREVISION=`svn info svn://svn.osgdc.org/kusu-repo/sandbox/kusu/ | grep Revision | awk '{print $2}'`
+KUSUREVISION=`svn info ${CMAKE_CURRENT_SOURCE_DIR} | grep Revision | awk '{print $2}'`
 boot-media-tool make-patch kususrc=${CMAKE_CURRENT_SOURCE_DIR} os=${KUSU_BUILD_DIST} version=${KUSU_BUILD_DISTVER} arch=${KUSU_BUILD_ARCH} patch=updates.img
 boot-media-tool make-iso kususrc=${CMAKE_CURRENT_SOURCE_DIR} source=${KUSU_DISTRO_SRC} arch=${KUSU_BUILD_ARCH} iso=kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.iso  patch=updates.img
 rm -f updates.img
+tar -czvf kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.tgz ${CMAKE_CURRENT_SOURCE_DIR}
+
