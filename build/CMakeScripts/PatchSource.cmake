c6a8175d0e6c6ef9e6ca2a11326e13ad51dab968
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-05-28 05:16:39 +0000

    resolve bug #KUSU-71

---
+++ build/CMakeScripts/PatchSource.cmake
@@ -0,0 +1,23 @@
+#search for .patch files in custom dir
+#apply patch 
+
+FIND_PROGRAM(PATCH
+  NAMES patch
+  PATHS
+  /bin
+  /usr/bin
+  /usr/local/bin
+)
+
+IF(PATCH)
+  FILE(GLOB PATCHFILES "${CMAKE_CURRENT_BINARY_DIR}/custom/${VERSION_STR}/*.patch")
+  IF(PATCHFILES)
+    FOREACH(patchfile ${PATCHFILES})
+      EXEC_PROGRAM(${PATCH} ${CMAKE_CURRENT_BINARY_DIR}/src
+        ARGS -p0 < ${patchfile}
+      )
+    ENDFOREACH(patchfile)
+  ENDIF(PATCHFILES)
+ELSE(PATCH)
+  MESSAGE( SEND_ERROR "patch executable not found. Skip building ${PROJECT_NAME}.")
+ENDIF(PATCH)
