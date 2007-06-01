bd6ca0dc7020b0fec418cfcb3132169a1f2dcfa5
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-06-01 08:26:46 +0000

    initial unit testing for database #KUSU-100

---
+++ src/modules/core/src/test/database_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# $Id$
+#
+# Copyright 2007 Platform Computing Corporation.
+#
+# Licensed under GPL version 2; See LICENSE for details.
+
+from kusu.core import database as db
+from path import path
+import tempfile
+import os
+
+class TestSQLITE:
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        self.kusudb = path(self.tempdir) / 'kusu.db'
+
+        self.dbs =  db.DB('sqlite', db=self.kusudb)
+
+    def tearDown(self):
+        self.dbs.dropTables()
+
+        try:
+            os.unlink(self.kusudb)
+        except: pass
+        
+        try:
+            os.removedirs(self.tempdir)
+        except: pass
+
+        session = self.dbs.createSession()
+        session.clear()
+        session.close()
+
+    def testTableCreation(self):
+        self.dbs.createTables()
+
+        for table in self.dbs.mapTableClass.keys():
+            assert self.dbs.metadata.engine.has_table(table)
+
+    def testTableDrop(self):
+        self.dbs.dropTables()
+
+        for table in self.dbs.mapTableClass.keys():
+            assert not self.dbs.metadata.engine.has_table(table)
+
+    def testBootstrap(self):
+        self.dbs.bootstrap()
+
+        session = self.dbs.createSession()
+
+        for name in ['installer', 'compute']:
+            ng = session.query(self.dbs.nodegroups).select_by(ngname=name)[0]
+            assert ng.ngname == name
