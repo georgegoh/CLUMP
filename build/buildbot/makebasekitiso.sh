4b922c97714993105bb07c01754d7a4287485b28
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-06-04 04:59:53 +0000

    catch exit status in buildbot make scripts. timestamp base kit iso.

--- build/buildbot/makebasekitiso.sh
+++ build/buildbot/makebasekitiso.sh
@@ -1,10 +1,22 @@
 #!${BASH_EXE}
 
 mkdir -m 755 -p ${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}
-cd ${CMAKE_CURRENT_SOURCE_DIR}/src/kits/base
+cd ${CMAKE_CURRENT_BINARY_DIR}/src/kits/base
 cp kit-base.spec kit-base.spec.orig
 sed -e '/^\%exclude/d' kit-base.spec.orig > kit-base.spec
 make
+ec=$?
+if [ $ec != "0" ]
+then
+  exit $ec
+fi
+export BUILDDATE=`date +"%Y%m%d%H%m%S"`
+export KUSUREVISION=`svn info ${CMAKE_CURRENT_SOURCE_DIR} | grep Revision | awk '{print $2}'`
+for i in `ls kit-base-*.iso`
+do
+  isoname=`echo $i | sed -e 's/noarch.iso//'`
+  mv $i $isoname$BUILDDATE-$KUSUREVISION.noarch.iso
+done
 chmod 644 *.iso
 cp *.iso /data/scratch/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/.
 rsync -av --rsh=ssh --delete /data/scratch/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH} build@ronin:/home/osgdc/www/pub/build/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/.
--- build/buildbot/makeiso.sh
+++ build/buildbot/makeiso.sh
@@ -5,7 +5,17 @@ source ${KUSU_ROOT}/bin/kusudevenv.sh
 BUILDDATE=`date +"%Y%m%d%H%m%S"`
 KUSUREVISION=`svn info ${CMAKE_CURRENT_SOURCE_DIR} | grep Revision | awk '{print $2}'`
 boot-media-tool make-patch kususrc=${CMAKE_CURRENT_SOURCE_DIR} os=${KUSU_BUILD_DIST} version=${KUSU_BUILD_DISTVER} arch=${KUSU_BUILD_ARCH} patch=updates.img
+ec=$?
+if [ $ec != "0" ]
+then
+  exit $ec
+fi
 boot-media-tool make-iso kususrc=${CMAKE_CURRENT_SOURCE_DIR} source=${KUSU_DISTRO_SRC} arch=${KUSU_BUILD_ARCH} iso=${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.iso  patch=updates.img
+ec=$?
+if [ $ec != "0" ]
+then
+  exit $ec
+fi
 rm -f updates.img
 tar -czf ${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.src.tgz ${CMAKE_CURRENT_SOURCE_DIR}
 chmod 644 ${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/kusu-$BUILDDATE-$KUSUREVISION.${KUSU_BUILD_DIST}-${KUSU_BUILD_DISTVER}.${KUSU_BUILD_ARCH}.*
