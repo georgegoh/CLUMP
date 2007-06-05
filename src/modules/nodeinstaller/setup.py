2d9bf241826ec85ba494caa5bc52aee1910bda3b
najib <najib@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-06-05 15:57:30 +0000

    Adding stub for the Kusu Client Node Installer.

---
+++ src/modules/nodeinstaller/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# $Id$
+#
+# Copyright 2007 Platform Computing Corporation.
+#
+# Licensed under GPL version 2; See LICENSE file for details.
+#
+
+from distutils.core import setup
+setup(name="kusu-nodeinstaller",
+      version="0.1",
+      author="Najib Ninaba",
+      author_email="najib@osgdc.org",
+      url="http://www.osgdc.org/project/kusu",
+      platforms=["any"],
+      packages = ['nodeinstaller'],
+      package_dir={'nodeinstaller': 'src/lib'},
+      scripts=['src/bin/nodeinstaller']
+     )
---
+++ src/modules/nodeinstaller/src/test/test_niidata.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# $Id$
+#
+# Copyright 2007 Platform Computing Corporation.
+#
+# Licensed under GPL version 2; See LICENSE file for details.
+#
+
+class TestNII:
+
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+        
+    def testNIIStructure(self):
+        assert True
+        
+    def testGetNII(self):
+        assert True
+        
+    def testFailGetNII(self):
+        assert True
+        
+    def testGetNodeInfoLine(self):
+        
+        # parse NII
+        # get nodeinfo
+        # validate nodeinfo
+        
+        assert True
+        
+    def testGetNodeInfoLine(self):
+        
+        # parse NII
+        # get nodeinfo
+        # validate nodeinfo
+        
+        assert True
+
+    def testGetNicInfoLine(self):
+        
+        # parse NII
+        # get nicinfo
+        # validate nicinfo
+        
+        assert True
+
+    def testGetPartitionInfoLine(self):
+        
+        # parse NII
+        # get partitioninfo
+        # validate partitioninfo
+        
+        assert True
+
+    def testGetComponentLine(self):
+        
+        # parse NII
+        # get component
+        # validate component
+        
+        assert True
+
+    def testGetPackageLine(self):
+        
+        # parse NII
+        # get package
+        # validate package
+        
+        assert True
+
+    def testGetScriptLine(self):
+        
+        # parse NII
+        # get script
+        # validate script
+        
+        assert True
+        
+    def testGetAppGlobalsLine(self):
+
+        # parse NII
+        # get appglobals
+        # validate appglobals
+        
+        assert True
+        
+    
+        
+
+
+
+    
