15dd2c12110a2103060ed7f6789c60f09898cf0b
mblack <mblack@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-06-20 18:01:45 +0000

    Fixed hostname for case when there is no suffix

--- src/kits/base/plugins/genconfig/dhcpd.py
+++ src/kits/base/plugins/genconfig/dhcpd.py
@@ -115,7 +115,10 @@ class thisReport(Report):
                         # Test to see if this nodes IP lies on the same network
                         # as this DHCP section
                         if onNetwork(netmask, subnet, row[1]):
-                            print '\thost %s-%s {' % (row[0], row[3])
+                            if row[3] and row[3] != '':
+                                print '\thost %s%s {' % (row[0], row[3])
+                            else:
+                                print '\thost %s {' % row[0]
                             print '\t\thardware ethernet %s;' % row[2]
                             print '\t\toption host-name "%s";' % row[0]
                             print '\t\tfixed-address %s;' % row[1]
