8890d979033cbfeeabb84c9d1efc1183191cea99
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-06-20 02:26:56 +0000

    add cheetah and pysqlite to base kit.

--- build/buildbot/makebasekitiso.sh
+++ build/buildbot/makebasekitiso.sh
@@ -12,19 +12,24 @@ do
       -e 's%RPM_BUILD_ROOT/repo%RPM_BUILD_ROOT/depot%g' \
       $i.orig > $i
 done
+
+cp packages/pdsh/Makefile packages/pdsh/Makefile.orig
+sed -e 's%rpmbuild --rebuild%rpmbuild --rebuild --without readline%' \
+packages/pdsh/Makefile.orig > packages/pdsh/Makefile
+
 make
 ec=$?
 if [ $ec != "0" ]
 then
   exit $ec
 fi
-export BUILDDATE=`date +"%Y%m%d%H%m%S"`
-export KUSUREVISION=`svn info ${CMAKE_CURRENT_SOURCE_DIR} | grep Revision | awk '{print $2}'`
-for i in `ls kit-base-*.iso`
-do
-  isoname=`echo $i | sed -e 's/noarch.iso//'`
-  mv $i $isoname$BUILDDATE-$KUSUREVISION.noarch.iso
-  chmod 644 $isoname$BUILDDATE-$KUSUREVISION.noarch.iso
-done
-cp -p *.iso /data/scratch/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/.
-#rsync -av --rsh=ssh --delete /data/scratch/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH} build@ronin:/home/osgdc/www/pub/build/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/.
+# export BUILDDATE=`date +"%Y%m%d%H%m%S"`
+# export KUSUREVISION=`svn info ${CMAKE_CURRENT_SOURCE_DIR} | grep Revision | awk '{print $2}'`
+# for i in `ls kit-base-*.iso`
+# do
+#   isoname=`echo $i | sed -e 's/noarch.iso//'`
+#   mv $i $isoname$BUILDDATE-$KUSUREVISION.noarch.iso
+#   chmod 644 $isoname$BUILDDATE-$KUSUREVISION.noarch.iso
+# done
+# cp -p *.iso /data/scratch/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/.
+# rsync -av --rsh=ssh --delete /data/scratch/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH} build@ronin:/home/osgdc/www/pub/build/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/.
