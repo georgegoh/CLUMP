40f5b49806b6278daa851df8d85a777c09b2b897
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-07-04 14:48:50 +0000

    save to database. On going work for #KUSU-188

--- src/modules/installer/src/lib/rhel_instnum.py
+++ src/modules/installer/src/lib/rhel_instnum.py
@@ -10,20 +10,26 @@
 
 import snack
 from gettext import gettext as _
+from kusu.util import profile
 from kusu.ui.text import kusuwidgets
 from kusu.ui.text.kusuwidgets import LEFT,CENTER,RIGHT
 import kusu.util.log as kusulog
 from screen import InstallerScreen
 from kusu.ui.text.navigator import NAV_FORWARD
+from path import path
 import instnum
 
 kl = kusulog.getKusuLog('installer.instnum')
 
-class LicenseScreen(InstallerScreen):
-    name = _('Installation Number')
+class LicenseScreen(InstallerScreen, profile.PersistentProfile):
+    name = _('Inst Number')
     profile = 'InstNum'
     msg = _('Please enter the Installation number')
     buttons = [_('Skip')]
+
+    def __init__(self, kiprofile):
+        InstallerScreen.__init__(self, kiprofile=kiprofile)
+        profile.PersistentProfile.__init__(self, kiprofile)        
 
     def setCallbacks(self):
         """
@@ -38,11 +44,14 @@ class LicenseScreen(InstallerScreen):
         return NAV_FORWARD
 
     def drawImpl(self):
+        
+        if not self.kiprofile.has_key(self.profile): self.setDefaults()
+
         self.screenGrid = snack.Grid(1, 2)
         entryWidth = 20
         self.license = kusuwidgets.LabelledEntry(
                        labelTxt=_('Installation number ').rjust(26), 
-                       width=entryWidth)
+                       width=entryWidth, text=self.kiprofile[self.profile])
 
         self.screenGrid.setField(snack.TextboxReflowed(text=self.msg,
                                                        width=self.gridWidth),
@@ -58,7 +67,11 @@ class LicenseScreen(InstallerScreen):
         try:
             inum = instnum.InstNum(key)
         except:
-            return False, 'Invalid installation number'
+            errMsg = _("Invalid installation number\n\n" \
+                       "If you're unable to locate\n" \
+                       "the Installation Number, consult\n" \
+                       "http://www.redhat.com/apps/support/in.html.")
+            return False, errMsg
 
         if inum:
             # make sure the base products match
@@ -70,13 +83,21 @@ class LicenseScreen(InstallerScreen):
                 f.close()
 
                 if inum.get_product_string().lower() != productPath.lower():
-                    return False, "Installation number incompatible with media"
+                    return False, _('Installation number incompatible with media')
     
         return True, ''
- 
+
+    def setDefaults(self):
+        self.kiprofile[self.profile] = ''
+
     def formAction(self):
         """
         Store
         """
 
         self.kiprofile[self.profile] = self.license.value()
+
+    def save(self, db, profile):
+        newag = db.AppGlobals(kname=self.profile, kvalue=profile)
+        newag.save()
+        db.flush()
