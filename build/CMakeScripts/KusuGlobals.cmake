d8288fb4eb6dfbe8f9df0062a93baf0dd97465f0
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-07-05 08:15:32 +0000

    fix KUSU-185. unify kusuenv.sh files. now only edit src/modules/core/src/bin/kusuenv.sh. all others will be copied.

--- build/CMakeScripts/KusuGlobals.cmake
+++ build/CMakeScripts/KusuGlobals.cmake
@@ -158,7 +158,9 @@ CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/bin/kusudevenv.sh
 
 IF(KUSU_BUILD_DIST AND KUSU_BUILD_DISTVER)
   CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/src/modules/core/src/bin/kusuenv.sh 
-${CMAKE_CURRENT_BINARY_DIR}/src/modules/core/src/bin/kusuenv.sh)
+  ${CMAKE_CURRENT_BINARY_DIR}/src/dists/${KUSU_BUILD_DIST}/${KUSU_BUILD_DISTVER}/${KUSU_BUILD_ARCH}/kusuenv.sh)
+  CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/src/modules/core/src/bin/kusuenv.sh 
+  ${CMAKE_CURRENT_BINARY_DIR}/src/modules/core/src/bin/kusuenv.sh)
 
   CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/bin/makeiso.sh ${CMAKE_CURRENT_BINARY_DIR}/bin/makeiso.sh)
   CONFIGURE_FILE(${CMAKE_CURRENT_BINARY_DIR}/bin/makebasekit.sh                
