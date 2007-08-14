caf3c5a7e0f8ab5d402828a7234d46407d4a7ef4
hirwan <hirwan@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-08-14 01:43:26 +0000

    use /etc/hosts.pdsh instead of /etc/hosts-pdsh

--- src/kits/base/plugins/addhost/05-hostspdsh.py
+++ src/kits/base/plugins/addhost/05-hostspdsh.py
@@ -17,10 +17,10 @@ import os
 
 class AddHostPlugin:
     def added(self, dbconn, nodename, info):
-        os.system("/opt/kusu/bin/genconfig hostspdsh > /etc/hosts-pdsh")
+        os.system("/opt/kusu/bin/genconfig hostspdsh > /etc/hosts.pdsh")
 
     def removed(self, dbconn, nodename, info):
-        os.system("/opt/kusu/bin/genconfig hostspdsh > /etc/hosts-pdsh")
+        os.system("/opt/kusu/bin/genconfig hostspdsh > /etc/hosts.pdsh")
 
     def updated(self, dbconn, nodename, info):
-        os.system("/opt/kusu/bin/genconfig hostspdsh > /etc/hosts-pdsh")
+        os.system("/opt/kusu/bin/genconfig hostspdsh > /etc/hosts.pdsh")
