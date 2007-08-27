3a58561a6cbeb51e7bb364f9b7de159786d313f5
spstarr <spstarr@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-08-27 22:56:23 +0000

    Change Addhost plugin mechanism. Alex's suggestions make this much cleaner. Needed because we need we do not want to run all the plugins for certain kits not specified for a nodegroup. the enabled() method will check if the kit needs the plugin to be executed.

---
+++ src/kits/base/packages/kusu-base-installer/lib/kusu/addhost.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#
+# Kusu add host
+#
+# Copyright (C) 2007 Platform Computing Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+# Author: Shawn Starr <sstarr@platform.com>
+
+""" AddHostPluginBase: These class methods may be overwriteable as needed """
+
+class AddHostPluginBase:
+      def __init__(self, dbconn):
+          self.dbconn = dbconn
+
+      def finished(self, nodelist):
+          """virtual"""
+          pass
+
+      def added(self, nodename, info):
+          """virtual"""
+          pass
+
+      def deleted(self, nodename, info):
+          """virtual"""
+          pass
+
+      def replaced(self, nodename, info):
+          """virtual"""
+          pass
+
+      def updated(self):
+          """virtual"""
+          pass
+
+      def enabled(self):
+          """virtual"""
+          return True
