1ab07be5f9f5e53bb67fea85a2ecc3335723f7f4
spstarr <spstarr@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-08-30 14:53:42 +0000

    Fix command option

--- src/kits/base/packages/kusu-base-installer/sbin/addhost.py
+++ src/kits/base/packages/kusu-base-installer/sbin/addhost.py
@@ -371,7 +371,7 @@ class AddHostApp(KusuApp):
                       except:
                           print kusuApp._("Error: The value %s is not a number. Please try again" % response)
                           flag = 1
-        else: 
+        elif (haveNodegroup and self._options.macfile and self._options.interface):
              self.parser.error(kusuApp._("Cannot use -i with -f and -n options"))
 
         # If nodegroup format and rack specified but node format does not have a rack AND rank. Ignore user set rack and use 0.
