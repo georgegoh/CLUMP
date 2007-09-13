48b94c9204a1cccaa9d0f92778e6c2ed2e175543
najib <najib@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-09-13 03:18:59 +0000

    Ongoing work on KUSU-366.

---
+++ src/kits/hpc/sources/00-postun-script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Code necessary to cleanup the database from any entries inserted by the %post
+
+# Put any custom stuff after this line
+# SQL/Shell/Python code to update the database.. The updates may optionally
+# include Node group creation and component association
+
+# sqlrunner may be used to perform sql injections
+# ngedit may be used non-interactively to add and copy nodegroups
