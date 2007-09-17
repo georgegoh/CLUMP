cdbc2374462765f320dc7faab725a23e26d2701f
mike <mike@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2007-09-17 08:08:39 +0000

    Use KusuDB for hostspdsh plugin. KUSU-374.

--- src/kits/base/plugins/genconfig/hostspdsh.py
+++ src/kits/base/plugins/genconfig/hostspdsh.py
@@ -17,14 +17,34 @@ from kusu.genconfig import Report
 
 class thisReport(Report):
     def runPlugin(self, pluginargs):
-        self.dbs = db.DB('mysql', 'kusudb', 'root')
-        piname = self.dbs.AppGlobals.selectfirst_by(kname='PrimaryInstaller').kvalue
+        query = ("SELECT nodes.name, networks.suffix " +
+                 "FROM nodes, nics, networks WHERE nics.nid = nodes.nid " +
+                 "AND nics.netid = networks.netid " +
+                 "AND nodes.name != (SELECT kvalue FROM appglobals WHERE " +
+                 "                  kname='PrimaryInstaller') " +
+                 "AND networks.type = 'provision'")
 
-        nodes = self.dbs.Nodes.select()
-        
-        for node in nodes:
-            if node.name == piname:
-                continue
-            for nic in node.nics:
-                if nic.network.type == 'provision':
-                    print "%s%s" % (node.name, nic.network.suffix)
+        try:
+            self.db.execute(query)
+        except:
+            sys.stderr.write(self.gettext("DB_Query_Error\n"))
+            sys.exit(-1)
+        else:
+            data = self.db.fetchall()
+            for row in data:
+                print "%s%s" % row
+
+        #self.dbs = db.DB('mysql', 'kusudb', 'root')
+        #pi = self.dbs.AppGlobals.selectfirst_by(kname='PrimaryInstaller').kvalue
+
+        #stmt = db.sa.select([self.dbs.nodes.c.name, self.dbs.networks.c.suffix])
+        #stmt.append_from(
+        #    self.dbs.nodes.join(self.dbs.nics,
+        #                    self.dbs.nodes.c.nid == self.dbs.nics.c.nid).join(
+        #                    self.dbs.networks,
+        #                    self.dbs.nics.c.netid == self.dbs.networks.c.netid))
+        #stmt.append_whereclause(self.dbs.nodes.c.name != pi)
+        #stmt.append_whereclause(self.dbs.networks.c.type == 'provision')
+
+        #for row in stmt.execute().fetchall():
+        #    print "%s%s" % (row.name, row.suffix)
