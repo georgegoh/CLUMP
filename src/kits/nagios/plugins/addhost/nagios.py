3f94bb033e76fa30deb672877abdef7c83fbb464
mike <mike@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2008-01-07 09:48:59 +0000

    -use cfm to distribute nagios monitoring node IPs to monitored nodes; KUSU-749 -do not wait for cfmsync to finish

--- src/kits/nagios/plugins/addhost/nagios.py
+++ src/kits/nagios/plugins/addhost/nagios.py
@@ -43,8 +43,31 @@ class AddHostPlugin(AddHostPluginBase):
             pickled = cPickle.dump(ngs, f)
             f.close()
 
+        # Grab the IPs of any nagios monitoring nodes.
+        self.dbconn.execute("SELECT nics.ip " +
+                        "FROM nics, nodes, ng_has_comp, components " +
+                        "WHERE nics.nid = nodes.nid " +
+                        "AND nodes.ngid = ng_has_comp.ngid " +
+                        "AND ng_has_comp.cid = components.cid " +
+                        "AND components.cname = 'component-installer-nagios'")
+        nagios_ips = ' '.join([x[0] for x in self.dbconn.fetchall()])
+
+        # Which nodegroups have component-compute-nagios?
+        self.dbconn.execute("SELECT nodegroups.ngname " +
+                        "FROM nodegroups, ng_has_comp, components " +
+                        "WHERE nodegroups.ngid = ng_has_comp.ngid " +
+                        "AND ng_has_comp.cid = components.cid " +
+                        "AND components.cname = 'component-compute-nagios'")
+        nagios_ngs = self.dbconn.fetchall()
+        for nagios_ng in nagios_ngs:
+            f = open('/etc/cfm/%s/etc/nagios_monitor_ips.cfg' % nagios_ng[0], 'w')
+            f.write(nagios_ips)
+            f.close()
+
         # Now, sync the pickled file to all the nodes
         cmds = ['cfmsync', '-f']
         cfmsyncP = subprocess.Popen(cmds, stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
-        out, err = cfmsyncP.communicate()
+        # We launch the cfmsync process and cross our fingers it completes;
+        # we won't wait until it's done.
+        #out, err = cfmsyncP.communicate()
