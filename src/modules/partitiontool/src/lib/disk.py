4ef757e8d809dd69012550517f69a79f4e5d7a57
ggoh <ggoh@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2008-03-15 10:29:27 +0000

    Show dell UP flag.

--- src/modules/partitiontool/src/lib/disk.py
+++ src/modules/partitiontool/src/lib/disk.py
@@ -719,7 +719,8 @@ class Partition(object):
         s = '  ' + self.path + ' size(MB): ' + str(self.size_MB) + '\n'
         s += '  start sector: ' + str(self.start_sector) + ' end sector: ' + \
              str(self.end_sector) + ' FS: ' + str(self.fs_type) + \
-             ' Leave unchanged: ' + str(self.leave_unchanged)
+             ' Leave unchanged: ' + str(self.leave_unchanged) + '\n'
+        s += ' Partition type: ' + self.type + ' dellUP_flag: ' + str(self.dellUP_flag)
         return s
 
     def __init__(self, disk, pedPartition, mountpoint=None):
