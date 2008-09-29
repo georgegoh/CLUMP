59a0359d8ca2de0eea12b31dec2560bab359b03f
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2008-09-29 07:34:35 +0000

    Fix kusu release name

--- config.mk
+++ config.mk
@@ -24,6 +24,7 @@ KUSU_DISTRO_ARCH=x86_64
 KUSU_TOPDIR=`pwd`
 KUSU_RPM_TMPPATH=/tmp/kusu
 
+KUSU_RELEASE_NAME=Kramat
 KUSU_VERSION = 0.10
 KUSU_REVISION = $(shell svn info $(KUSU_TOPDIR) | grep "Last Changed Rev" | awk '{print $$4}')
 
