50c3a3507bc43b1cd57ca5c1efb3acc00e1c6584
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2008-10-07 16:21:16 +0000

    Fix KUSU_TMP

--- src/dists/centos/5/x86_64/updates.img/thttpd-run.sh
+++ src/dists/centos/5/x86_64/updates.img/thttpd-run.sh
@@ -3,5 +3,5 @@
 # thttpd has been unreliable lately; don't let it give up!
 while [ 1 ]
 do
-    /tmp/updates/usr/sbin/thttpd -D -nor -nos -p 80 -h 127.0.0.1 -u root -d /tmp/kusu/www -l /dev/null
+    /tmp/updates/usr/sbin/thttpd -D -nor -nos -p 80 -h 127.0.0.1 -u root -d /tmp/www -l /dev/null
 done
