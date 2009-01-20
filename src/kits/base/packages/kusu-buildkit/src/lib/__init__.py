d9aa37b4fe45f4cbebf1780e8f50693a0f582221
ggoh <ggoh@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2009-01-20 03:14:44 +0000

    KUSU-1137. Fixed the __init__.py to reflect filename changes.

--- src/kits/base/packages/kusu-buildkit/src/lib/__init__.py
+++ src/kits/base/packages/kusu-buildkit/src/lib/__init__.py
@@ -5,10 +5,7 @@
 #
 # Licensed under GPL version 2; See LICENSE for details.
 
-from kusu.buildkit.kitsource import KitSrcFactory
 from kusu.buildkit.methods  import *
 from kusu.util.tools import getArch
-from kusu.buildkit.tool import BuildKit
 from kusu.buildkit.builder import setupRPMMacrofile, prepareNS, PackageProfile, getBuildKitTemplate
 from kusu.buildkit.checker import getSyntaxValidator
-
