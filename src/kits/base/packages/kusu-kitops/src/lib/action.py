d9b85a3dfa34f44f38ff26f695a68ff3af58ac4b
mike <mike@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2009-05-21 06:11:55 +0000

    Initial implementation of kit upgrades
    In the current state, a kit can be upgraded (replaced) with a newer kit,
    maintianing repo and nodegroup associations. Preservation of the
    associations is very rudimentary and makes many assumptions. Much work
    lies ahead.
    
    This commit also removes trailing whitespace.
    
    Work for KUSU-1264. Reviewed in KUSU-REVIEW-191.

--- src/kits/base/packages/kusu-kitops/src/lib/action.py
+++ src/kits/base/packages/kusu-kitops/src/lib/action.py
@@ -1,19 +1,129 @@
+import types
+import rpm
+from kusu.util.errors import UpdateKitError
+from kusu.kitops.addkit_strategies import AddKitStrategy
+from kusu.kitops.deletekit_strategies import DeleteKitStrategy
+from kusu.repoman.repofactory import RepoFactory
+
 class KitopsAction(object):
     """Head of class hierarchy implementing actions on kits.
 
        Descendants of this class implement actions such as adding, updating or
        removing kits."""
 
-    def __init__(self, koinst):
+    def __init__(self, db, koinst):
         """Create a KitopsAction instance.
 
            koinst is an instance of KitOps"""
 
         super(KitopsAction, self).__init__()
 
+        self._db = db
         self.koinst = koinst
 
-    def run(self):
+    def run(self, **kw):
         """Perform the action."""
 
         raise NotImplementedError
+
+class UpdateAction(KitopsAction):
+    def run(self, **kw):
+        """Perform the action."""
+
+        # Find the kit with the specified id. We assume there is only one.
+        old_kit_id = kw.pop('old_kit_id')
+        old_kit = self._db.Kits.get(old_kit_id)
+
+        # Determine which kit(s) in the supplied kit source (media, repo, etc)
+        # can be updates.
+        possible_kits = []
+        available_kits = kw.pop('kits', [])
+        for kit in available_kits:
+            # We have one level of indirection here...
+            kit_api = kit[4]
+            kitinfo = kit[1]
+            # We only select kits that meet the following criteria:
+            # [ ] have the same name (kit.rname)
+            # [ ] have the same arch (kit.arch)
+            # [ ] are newer than the kit being updated (kit.version/kit.release)
+            # [ ] the new kit's API is supported
+            if kitinfo['pkgname'] == 'kit-%s' % old_kit.rname and kitinfo['arch'] == old_kit.arch \
+                    and isNewer(old_kit.version, old_kit.release, kitinfo['version'], kitinfo['release']) \
+                    and AddKitStrategy.has_key(kit_api) and DeleteKitStrategy.has_key(kit_api):
+                possible_kits.append(kit)
+
+        # TODO: Ask the user which new kit to use?
+        if possible_kits: kit_to_add = possible_kits[0]
+        else:
+            raise UpdateKitError, 'No suitable kits avaialble for upgrade'
+
+        # TODO:
+        # Check min_version to determine whether a kit can be upgraded
+
+        # Get a list of repos the old kit was associated with. We will need to
+        # associate the new kit with these repos.
+        associated_repo_ids = [r.repoid for r in self._db.ReposHaveKits.select_by(kid=old_kit_id)]
+
+        # Add the new kit, and pull it from the DB
+        kit_api = kit_to_add[4]
+        new_kit_id, updated_ngs = AddKitStrategy[kit_api](self.koinst, self._db, kit_to_add)
+        new_kit = self._db.Kits.get(new_kit_id)
+
+        # Let's re-associate repos
+        for repo_id in associated_repo_ids:
+            repo = self._db.Repos.get(repo_id)
+
+            # First, deassociate the old kit from the repo
+            for i in xrange(len(repo.kits)):
+                if old_kit_id == repo.kits[i].kid:
+                    repo.kits.pop(i)
+                    break
+
+            # TODO: check whether the new kit has compatible components
+            # Add the new kit
+            repo.kits.append(new_kit)
+            RepoFactory(self._db).getRepo(repo_id).markStale()
+
+        # Now we handle the nodegroup-component associations
+        for new_component in new_kit.components:
+            for old_component in old_kit.components:
+                if old_component.cname == new_component.cname:
+                    new_component.nodegroups = old_component.nodegroups
+                    old_component.nodegroups = []
+
+        old_kit = self._db.Kits.get(old_kit_id)
+        DeleteKitStrategy[self.koinst.getKitApi(old_kit_id)](self.koinst, self._db, old_kit)
+        self._db.flush()
+
+        # TODO:
+        # ...
+        # Obtain user confirmation
+        # Run pre-section of kit upgrade plugin
+        # Replace the old components with new ones accordingly
+        # Run post section of kit upgrade plugin
+        # Propmt user to run cfmsync --upgrade
+
+
+def isNewer(old_ver, old_rel, new_ver, new_rel):
+    """Compares old and new to determine whether new is newer.
+
+    old_ver is the old version
+    old_rel is the old release
+    new_ver is the new version
+    new_rel is the new release
+
+    returns true if new is newer than old, false otherwise.
+    """
+
+    def toStr(a):
+        if type(a) != types.StringType and a != None:
+            a = str(a)
+        return a
+
+    old_ver = toStr(old_ver)
+    old_rel = toStr(old_rel)
+
+    new_ver = toStr(new_ver)
+    new_rel = toStr(new_rel)
+
+    return -1 == rpm.labelCompare(("0", old_ver, old_rel), ("0", new_ver, new_rel))
