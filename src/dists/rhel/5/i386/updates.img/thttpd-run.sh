3134000f76e51fcb8caf10b1fe507abf0840b8a6
ltsai <ltsai@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2010-04-08 14:16:17 +0000

    bye bye

--- src/dists/rhel/5/i386/updates.img/thttpd-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# thttpd has been unreliable lately; don't let it give up!
-while [ 1 ]
-do
-    /tmp/updates/usr/sbin/thttpd -D -nor -nos -p 80 -h 127.0.0.1 -u root -d /tmp/www -l /dev/null
-done
