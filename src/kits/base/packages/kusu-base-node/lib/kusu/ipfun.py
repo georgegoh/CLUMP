3193a6ba5a8fb2c85efdf6f110592487595bb027
mkchew <mkchew@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2010-04-26 09:48:51 +0000

    Correct spelling mistakes. Correct indentation mistakes. Remove method signatures from method doc strings.

--- src/kits/base/packages/kusu-base-node/lib/kusu/ipfun.py
+++ src/kits/base/packages/kusu-base-node/lib/kusu/ipfun.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
+#
+# $Id$
+#
 # ipfun.py - A collection of functions for manipulating IP addresses
 
 #   Copyright 2007 Platform Computing Inc
@@ -24,9 +26,9 @@ import string
 import os
 
 def validIP(IP):
-    """validIP - Test the IP address to see if it is a valid format.
-                 Returns:   0  - IP address is invalid.
-                 1  - IP address is good."""
+    """Test the IP address to see if it is a valid format.
+    Returns:   0  - IP address is invalid.
+               1  - IP address is good."""
     substr = string.split(IP, '.')
     if len(substr) != 4:
         return 0
@@ -48,9 +50,9 @@ def validIP(IP):
     
 
 def ip2number(IP):
-    """ip2number - Convert an IPv4 address into 32bit number.  The IP must
-                   pass the validIP() test prior to conversion.
-                   Returns:  The numeric equivilent of the IP address."""
+    """Converts an IPv4 address into 32bit number.
+    The IP must pass the validIP() test prior to conversion.
+    Returns:  The numeric equivalent of the IP address."""
     substr = string.split(IP, '.')
     numval = 0
     for num in substr:
@@ -60,8 +62,8 @@ def ip2number(IP):
 
 
 def number2ip(number):
-    """number2ip - Convert a 32bit integer into a IPv4 address. 
-                   Returns:  The IP address numerically equivilent to the number."""
+    """Converts a 32bit integer into a IPv4 address. 
+    Returns: The IP address numerically equivalent to the number."""
     ip = ''
     byte1 = number & 0xff
     byte2 = (number & 0xff00) >> 8
@@ -72,11 +74,11 @@ def number2ip(number):
 
                 
 def incrementIP(startIP, increment=1, subnet=''):
-    """incrementIP - Increment/decrement the startIP by the increment value.
-                     Optionally test the generated IP to ensure it does not
-                     overflow the subnet
-                     Returns:  New IP address
-                     Raises:  Exception of IP address overflow. """
+    """Increment/decrement the startIP by the increment value.
+    Optionally test the generated IP to ensure it does not
+    overflow the subnet.
+    Returns: New IP address
+    Raises:  Exception if IP address is not within the given subnet."""
     startIPnum = ip2number(startIP)
     newIPnum = startIPnum + increment
     
@@ -95,8 +97,8 @@ def incrementIP(startIP, increment=1, subnet=''):
 
 
 def onNetwork(network, subnet, ip, start_ip=None):
-    """onNetwork - Determine if the given ip lies within the network.
-    Requires the network address, on an IP on that network, as well as
+    """Determines if the given ip lies within the network.
+    Requires the network address, an IP on that network, as well as
     the subnet mask. Optional parameter is the pcm network start_ip to
     respect the PCM network schema where two overlapping networks have
     the same network and subnet, but different starting ips."""
@@ -120,21 +122,20 @@ def onNetwork(network, subnet, ip, start_ip=None):
 
 
 def getNetwork(ip, subnet):
-    """getNetwork - Get the network address given and IP, and the subnet mask"""
+    """Returns the network address given an IP and the subnet mask"""
     netnum = ip2number(ip) & ip2number(subnet)
     return number2ip(netnum)
 
 
 def getBroadcast(ip, subnet):
-    """getBroadcast- Get the broadcast address given and IP, and the subnet mask"""
+    """Returns the broadcast address given an IP and the subnet mask"""
     bcnum = ip2number(ip) | (~ip2number(subnet))
     return number2ip(bcnum)
 
 
 def getMyIPs ():
-    """getMyIPs - Determine the IP address(es) and subnet masks
-    of this machine.
-    WARNING:  This uses ifconfig to find the IP's"""
+    """Determine the IP address(es) and subnet masks of this machine.
+    WARNING:  This uses ifconfig to find the IPs"""
     myips = []
     ifconfig = '/sbin/ifconfig'
     cmd = "%s |grep inet" % ifconfig
@@ -156,20 +157,20 @@ def getMyIPs ():
 
 
 def bestIP (myIPs, ipList):
-        """bestIP - Determine which IP's in the ipList are on the
-        same network as this machine.  The 'myIPs' is the list of
-        IPs produced by the getMyIPs function.  Returns a list of
-        IP's from the ipList that are on the same network as this
-        machine, or an empty list."""
-        onnet = []
-        for ip in ipList:
-            for network, subnet in myIPs:
-                if onNetwork(network, subnet, ip):
-                    onnet.append(ip)
-        return onnet
+    """Determine which IP in the ipList are on the same network as 
+    this machine.  The 'myIPs' is the list of IPs produced by the
+    getMyIPs function.
+    Returns a list of IPs from the ipList that are on the same 
+    network as this machine, or an empty list."""
+    onnet = []
+    for ip in ipList:
+        for network, subnet in myIPs:
+            if onNetwork(network, subnet, ip):
+                onnet.append(ip)
+    return onnet
 
 def getArpaZone(network, mask):
-    """getArpaZone - Get the parent class subnet and its arpa zone"""
+    """Get the parent class subnet and its arpa zone"""
     ipbytes = string.split(network, '.')
     maskbytes = string.split(mask, '.')
 
@@ -229,7 +230,7 @@ def unittest():
         print outstr
         testnum += 1
 
-        # Negitive Test Cases
+        # Negative Test Cases
         outstr = "Test validIP() #%i" % testnum
         ip = "1.2.3.4.5"
         if validIP(ip) == 1:
