cc13f6e8e2742a1af832a7b106963598f37cd53e
mkchew <mkchew@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2010-07-14 05:57:16 +0000

    The '__all__' variable is required to expose the public names defined by a module. Reverting to the original version of __init__.py except for 'installextrakitreceiver' and 'systemsettingsreceiver' which have been removed in an earlier commit.

---
+++ src/kits/base/packages/kusu-setup/src/lib/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (C) 2010 Platform Computing Inc.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+
+__all__ = ['cleanupreceiver',
+'createosrepocommand',
+'createosreporeceiver',
+'cleanupcommand',
+'dhcpcheckcommand',
+'diskspacecheckcommand',
+'diskspacecheckreceiver',
+'envcheckcommand',
+'fqdnreceiver',
+'initkusudbcommand',
+'initkusudbreceiver',
+'installerinitreceiver',
+'installextrakitcommand',
+'installoskitcommand',
+'installoskitreceiver',
+'keyboardreceiver',
+'languagereceiver',
+'message',
+'networkreceiver',
+'niccheckcommand',
+'ramcheckcommand',
+'ramcheckreceiver',
+'rpmcheckcommand',
+'rpminstallcommand',
+'rpminstallreceiver',
+'sanitizeinstallcommand',
+'setup_errors',
+'selinuxcheckcommand',
+'systemsettingscommand',
+'timezonereceiver']
