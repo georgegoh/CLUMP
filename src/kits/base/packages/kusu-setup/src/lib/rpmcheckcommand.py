dd6440276408391633cfc00e02d2901983b8e28c
mkchew <mkchew@30a2dd0b-8a21-0410-827e-96ebc960adc3>
2010-07-14 05:50:29 +0000

    Use primitive's probe.OS instead of using /etc/issue, lsb_release, etc.
    This change allows the rpmcheckcommand to pass on SLC and SL.

--- src/kits/base/packages/kusu-setup/src/lib/rpmcheckcommand.py
+++ src/kits/base/packages/kusu-setup/src/lib/rpmcheckcommand.py
@@ -1,11 +1,29 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (C) 2010 Platform Computing Inc.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 from command import Command
 from string import upper
 import message
 
 #FIXME: This must be a build-time make macro
-SUPPORTED_OPERATING_SYSTEMS = ['RHEL', 'CENTOS']
+SUPPORTED_OPERATING_SYSTEMS = ['RHEL', 'CENTOS', 'SCIENTIFICLINUXCERN', 'SCIENTIFICLINUX']
 
 class RpmCheckCommand(Command):
     """
@@ -28,4 +46,4 @@ class RpmCheckCommand(Command):
             self._proceedStatus = False
             self._quitMessage = "This operating system is not compatible with this installer."
 
-        
+
